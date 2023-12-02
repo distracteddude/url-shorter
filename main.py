286f2ca9fa7a89002abc7021a637a3843e15d002
distracteddude <97978797+distracteddude@users.noreply.github.com>
2023-12-02 17:06:03 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pyshorteners
-
-long_url = input("Enter the url to short: ");
-
-tiny_url = pyshorteners.Shortener()
-
-short_url = tiny_url.tinyurl.short(long_url)
-
-print("The shortened url: " + short_url)
