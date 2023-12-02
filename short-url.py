a1d309d22c3d5798fe57c6995faa4eaad6deb0dd
distracteddude <harshitg187@gmail.com>
2023-12-02 16:56:28 +0530

    made the code look better

--- short-url.py
+++ short-url.py
@@ -2,21 +2,21 @@ import pyshorteners
 
 # create a func that short urls.
 def shorty(URL):
-    shortener= pyshorteners.Shortener()
+    shortener = pyshorteners.Shortener()
     shortened_url = shortener.tinyurl.short(URL)
     return shortened_url
 
 # create a loop that breaks when we put exit.
-active= True
+active = True
 
 while active:
-    url= input('Enter url: ')
+    url = input('Enter url: ')
 
-    if url.lower()=='exit':
-        active=False
+    if url.lower() =='exit':
+        active = False
 
     elif url:
-        shortened_url= shorty(url)
+        shortened_url = shorty(url)
         print(shortened_url)
     
     else:
